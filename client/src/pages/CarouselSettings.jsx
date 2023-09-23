ccb8942d920648ab39711313c50df4b1c1891909
LHoOXD <houssemhouss0@gmail.com>
2023-09-24 00:47:04 +0100

    adding spinner

--- client/src/pages/CarouselSettings.jsx
+++ client/src/pages/CarouselSettings.jsx
@@ -16,6 +16,7 @@ const CarouselSettings = () => {
     const [fileArray, setFileArray] = useState([]);
     const [itemToUpdate, setItemToUpdate] = useState();
     const [UpdateTime, setUpdateTime] = useState(false);
+    const [uploading, setUploading] = useState(false);
     const [rld, setRld] = useState(false);
 
     const imageRef = useRef(null);
@@ -40,6 +41,7 @@ const CarouselSettings = () => {
     });
 
     const handleFormSubmit = (data, onSubmitProps) => {
+        setUploading(true);
         console.log(data);
         const formData = new FormData();
 
@@ -54,12 +56,14 @@ const CarouselSettings = () => {
                 onSubmitProps.resetForm();
                 setRld(!rld);
                 setFileArray([]);
+                setUploading(false);
             })
             .catch((err) => {
                 console.log(err);
             });
     };
     const handleFormUpdate = (data, onSubmitProps) => {
+        setUploading(true);
         console.log(data);
         const formData = new FormData();
 
@@ -74,6 +78,7 @@ const CarouselSettings = () => {
                 onSubmitProps.resetForm();
                 setRld(!rld);
                 setFileArray([]);
+                setUploading(false);
             })
             .catch((err) => {
                 console.log(err);
@@ -87,7 +92,7 @@ const CarouselSettings = () => {
                     onSubmit={UpdateTime ? handleFormUpdate : handleFormSubmit}
                     validationSchema={validationSchema}
                     initialValues={UpdateTime ? itemToUpdate : initialValues}
-                    enableReinitialize    
+                    enableReinitialize
                 >
                     {({
                         values,
@@ -208,9 +213,28 @@ const CarouselSettings = () => {
                                             imageRef.current.click();
                                         }}
                                     >
-                                        Upload Image
+                                        Upload File
                                     </button>
                                 </Box>
+                                {uploading && (
+                                    <Box
+                                        sx={{
+                                            gridColumn: "span 4",
+                                            margin: "5px auto",
+                                        }}
+                                    >
+                                        <div class="lds-roller">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                    </Box>
+                                )}
 
                                 <div className="btn-container">
                                     <button
@@ -359,6 +383,91 @@ const Container = styled(Box)`
                 .button-56 {
                     padding: 0 40px;
                 }
+            }
+        }
+
+        .lds-roller {
+            display: inline-block;
+            position: relative;
+            width: 80px;
+            height: 80px;
+        }
+        .lds-roller div {
+            animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+            transform-origin: 40px 40px;
+        }
+        .lds-roller div:after {
+            content: " ";
+            display: block;
+            position: absolute;
+            width: 7px;
+            height: 7px;
+            border-radius: 50%;
+            background: #fff;
+            margin: -4px 0 0 -4px;
+        }
+        .lds-roller div:nth-child(1) {
+            animation-delay: -0.036s;
+        }
+        .lds-roller div:nth-child(1):after {
+            top: 63px;
+            left: 63px;
+        }
+        .lds-roller div:nth-child(2) {
+            animation-delay: -0.072s;
+        }
+        .lds-roller div:nth-child(2):after {
+            top: 68px;
+            left: 56px;
+        }
+        .lds-roller div:nth-child(3) {
+            animation-delay: -0.108s;
+        }
+        .lds-roller div:nth-child(3):after {
+            top: 71px;
+            left: 48px;
+        }
+        .lds-roller div:nth-child(4) {
+            animation-delay: -0.144s;
+        }
+        .lds-roller div:nth-child(4):after {
+            top: 72px;
+            left: 40px;
+        }
+        .lds-roller div:nth-child(5) {
+            animation-delay: -0.18s;
+        }
+        .lds-roller div:nth-child(5):after {
+            top: 71px;
+            left: 32px;
+        }
+        .lds-roller div:nth-child(6) {
+            animation-delay: -0.216s;
+        }
+        .lds-roller div:nth-child(6):after {
+            top: 68px;
+            left: 24px;
+        }
+        .lds-roller div:nth-child(7) {
+            animation-delay: -0.252s;
+        }
+        .lds-roller div:nth-child(7):after {
+            top: 63px;
+            left: 17px;
+        }
+        .lds-roller div:nth-child(8) {
+            animation-delay: -0.288s;
+        }
+        .lds-roller div:nth-child(8):after {
+            top: 56px;
+            left: 12px;
+        }
+        @keyframes lds-roller {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
             }
         }
     }

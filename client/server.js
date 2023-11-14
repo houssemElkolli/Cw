4723536a0e33f78918b9aed72efac9b611acf9b2
LHoOXD <houssemhouss0@gmail.com>
2023-11-14 12:36:43 +0100

    first

--- client/server.js
+++ client/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 
 // Constants
 // const isProduction = process.env.NODE_ENV === 'production'
-const isProduction = false 
+const isProduction = true 
 const port = process.env.PORT || 5173
 const base = process.env.BASE || '/'
 

e911e442c0fe496c6bcb4fcf91625023c8cff02c
LHoOXD <houssemhouss0@gmail.com>
2023-11-15 07:26:51 +0100

    first

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173", 
 "http://192.168.1.89:5173",
-"https://cwfront.onrender.com",
+// "https://cwfront.onrender.com",
 "https://cwfront2.onrender.com",
 
 ];
